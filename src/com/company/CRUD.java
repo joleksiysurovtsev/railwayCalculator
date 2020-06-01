ff8838163f879384396c364ee940dfc4349443d5
Oleksiy Surovtsev <joleksiysurovtsev@gmail.com>
2020-06-01 16:55:35 +0300

    added configurations for connecting to the server

--- src/com/company/CRUD.java
+++ src/com/company/CRUD.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class CRUD {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/station_db?useUnicode=true&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+
+
 }
