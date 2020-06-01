8c80a81f0b61c536b1b8af645ba6c386c25c8e1d
Oleksiy Surovtsev <joleksiysurovtsev@gmail.com>
2020-06-01 17:07:13 +0300

    add variables

--- src/com/company/CRUD.java
+++ src/com/company/CRUD.java
@@ -1,10 +1,22 @@
 package com.company;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class CRUD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/station_db?useUnicode=true&serverTimezone=UTC";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
+
+    private static Connection connection;
+    private static Statement statement;
+    private static ResultSet resultSet;
+
+    private BigDecimal cosTime;
+    private BigDecimal incCoef;
 
 
 
